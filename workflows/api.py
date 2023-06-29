278e50ded71785d51589a2bc94f902ffca657395
Dayra Leon <leonndayra@gmail.com>
2023-06-29 19:47:23 +0000

    update

--- workflows/api.py
+++ workflows/api.py
@@ -3,31 +3,42 @@ import pandas as pandas
 
 #API
 
-def get_info(playerID):
-    url = "https://mlb-data.p.rapidapi.com/json/named.player_teams.bam"
+def get_info(player_name):
+    url = f"https://www.balldontlie.io/api/v1/players/{player_name}"
     
     data = {
-        "player_id":"'493316'",
-        "season":"'2014'"
+        "id":237,
+        "first_name":"LeBron",
+         "last_name":"James",
+         "position":"F",
+         "height_feet": 6,
+         "height_inches": 8,
+         "weight_pounds": 250,
+        "team":{
+            "id":14,
+            "abbreviation":"LAL",
+            "city":"Los Angeles",
+            "conference":"West",
+            "division":"Pacific",
+            "full_name":"Los Angeles Lakers",
+            "name":"Lakers"
+        }
     }
 
-    headers = {
-	    "X-RapidAPI-Key": "90b564737fmsh9e5b3ab74e323adp1d1f7fjsn55583b924947",
-	    "X-RapidAPI-Host": "mlb-data.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=data)
+    response = requests.get(url,params=data)
     data = response.json()
+
+    name = 'Full Name: ' + data["first_name"] + ' ' + data["last_name"]
    
-    return data
+    return name
 
 
 def main():
-    playerID = input('Enter a MLB player ID: ')
-    player = get_info(playerID)
+    player_name = input('Enter a MLB player ID: ')
+    player = get_info(player_name)
 
     if player is not None:
-        print('The stats and games for {playerID} are {player}')
+        print(player)
     else:
         print('Player not found')
 
